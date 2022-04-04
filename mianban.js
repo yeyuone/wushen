e320fc602df908d04711b5b95dca3700827ec380
夜雨 <65315790+yeyuone@users.noreply.github.com>
2022-04-04 18:22:14 +0800

    Update mianban.js

--- mianban.js
+++ mianban.js
@@ -2,13 +2,17 @@
 
 var roleid =  document.querySelectorAll(".role-list>.select")[0].attributes['roleid'].value
 
-var v = "面板1.7更新内容:\n" +
-    "        1. 所有配置支持云备份\n" +
-    "        2. 目前功能:复制,触发隐藏,面板隐藏,快捷发言,颜色自定义\n"
-
-document.getElementsByClassName('content-message')[0].childNodes[0].insertAdjacentHTML("beforeend",'<ord>'+v+'</ord>')
-
-
+// var v = "面板1.7更新内容:\n" +
+//     "        1. 所有配置支持云备份\n" +
+//     "        2. 目前功能:复制,触发隐藏,面板隐藏,快捷发言,颜色自定义\n"
+//
+//
+// if (document.getElementsByClassName('content-message')[0].childNodes[0]){
+//     document.getElementsByClassName('content-message')[0].childNodes[0].insertAdjacentHTML("beforeend",'<ord>'+v+'</ord>')
+//
+// }else {
+//     document.getElementsByClassName('content-message')[0].insertAdjacentHTML("beforeend",'<ord>'+v+'</ord>')
+// }
 
 
 var styleStr = '.mianBanBegin {\n' +
